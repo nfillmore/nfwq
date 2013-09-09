8bfb27072de6c28a53837e0cd3d23307a8ea0ab4
Nathanael Fillmore <github@nate-fillmore.com>
2013-09-09 16:20:54 -0500

    Integrated start_{ssh,condor}_worker into nfwq.

--- nfwq.py
+++ nfwq.py
@@ -424,6 +424,130 @@ def update_state(argv):
   job_id = dag.tag_to_job_id(args.tag)
   dag.update_state(job_id, args.state)
 
+def start_ssh_worker(argv):
+
+  parser = argparse.ArgumentParser()
+  parser.add_argument("--server", required=True)
+  parser.add_argument("--port", required=True)
+  parser.add_argument("--client", required=True)
+  parser.add_argument("--frac_mem", type=float, default=0.85)
+  parser.add_argument("--frac_cpu", type=float, default=0.85)
+  parser.add_argument("--bindir")
+  parser.add_argument("--scratch", default="/scratch/nathanae/wq_tmp")
+  args = parser.parse_args(argv)
+
+  ls = subprocess.check_output(["ssh", args.client, "free", "-k"])
+  tot_mem = int(ls.decode("utf-8").split("\n")[1].split()[1])
+  max_mem = int(args.frac_mem * tot_mem)
+
+  ls = subprocess.check_output(["ssh", args.client, "cat", "/proc/cpuinfo"])
+  tot_cpu = 0
+  for l in ls.decode("utf-8").split("\n"):
+    if len(l.strip()) > 0:
+      k, v = l.split(":")
+      if k.strip() == "processor":
+        tot_cpu += 1
+  max_cpu = int(args.frac_cpu * tot_cpu)
+
+  if args.bindir is None:
+    args.bindir = os.path.dirname(subprocess.check_output(["which", "work_queue_worker"]).decode("utf-8").strip())
+
+  cmd = "ulimit -v {max_mem} && OMP_NUM_THREADS={max_cpu} {bindir}/work_queue_worker -t 900000000 -s {scratch} {server} {port}".format(
+    max_mem=max_mem, max_cpu=max_cpu, bindir=args.bindir, server=args.server, port=args.port, scratch=args.scratch)
+  print(cmd)
+  p = subprocess.Popen(["ssh", args.client, cmd])
+
+def start_condor_worker(argv):
+
+  parser = argparse.ArgumentParser()
+  parser.add_argument("--server", required=True)
+  parser.add_argument("--port", required=True)
+  parser.add_argument("--client", required=True)
+  parser.add_argument("--num_processes", type=int, default=1)
+  parser.add_argument("--frac_mem", type=float, default=0.85)
+  parser.add_argument("--frac_cpu", type=float, default=1.00)
+  parser.add_argument("--timeout", default="900s")
+  parser.add_argument("--bindir")
+  parser.add_argument("--logdir", default="/tier2/deweylab/scratch/nathanae/wq_condor_logs")
+  parser.add_argument("--internal_driver", action="store_true", help="For internal use only.")
+  args = parser.parse_args(argv)
+
+  if args.internal_driver:
+
+    ls = subprocess.check_output(["free", "-k"])
+    tot_mem = int(ls.decode("utf-8").split("\n")[1].split()[1])
+    max_mem = int(args.frac_mem * tot_mem)
+
+    ls = subprocess.check_output(["cat", "/proc/cpuinfo"])
+    tot_cpu = 0
+    for l in ls.decode("utf-8").split("\n"):
+      if len(l.strip()) > 0:
+        k, v = l.split(":")
+        if k.strip() == "processor":
+          tot_cpu += 1
+    max_cpu = int(args.frac_cpu * tot_cpu)
+
+    if args.bindir is None:
+      args.bindir = os.path.dirname(subprocess.check_output(["which", "work_queue_worker"]).decode("utf-8").strip())
+
+    subprocess.check_call(["pwd"])
+    subprocess.check_call(["env"])
+
+    tmpdir = os.environ["TMPDIR"]
+    print("tmpdir is {}".format(tmpdir))
+
+    cmd = "ulimit -v {max_mem} && OMP_NUM_THREADS={max_cpu} {bindir}/work_queue_worker -t {timeout} -s {tmpdir} {server} {port}".format(
+      max_mem=max_mem, max_cpu=max_cpu, bindir=args.bindir, timeout=args.timeout, tmpdir=tmpdir, server=args.server, port=args.port)
+    subprocess.check_call(cmd, shell=True)
+
+  else:
+
+    current_script = os.path.abspath(__file__)
+
+    if args.client[-4:] != ".edu":
+      args.client += ".biostat.wisc.edu"
+
+    l = subprocess.check_output(["condor_status", "-format", "%s,", "TotalCpus", args.client])
+    tot_cpu = int(l.decode("utf-8").split(",")[0])
+    max_cpu = int(args.frac_cpu * tot_cpu)
+
+    if args.num_processes == 0:
+      args.num_processes = max_cpu
+
+    # We set should_transfer_files = yes not because we want to use condor to
+    # transfer files (we don't), but because this forces condor to execute in the
+    # EXECUTE directory, which we do want. Cf.
+    # <http://comments.gmane.org/gmane.comp.distributed.condor.user/8396>. There
+    # doesn't seem to be a better way to achieve this.
+    submit = """
+      universe = vanilla
+      notification = never
+      getenv = True
+      should_transfer_files = no
+      #should_transfer_files = yes
+      #when_to_transfer_output = ON_EXIT_OR_EVICT
+      log = {logdir}/condor.log
+
+      requirements = (TARGET.Machine == "{client}")
+      #request_cpus = {max_cpu}
+
+      executable = {current_script}
+      arguments = start_condor_worker {args} --internal_driver 
+
+      output = {logdir}/condor.{client}.$(Process).out
+      error = {logdir}/condor.{client}.$(Process).err
+
+      queue {num_processes}
+    """.format(
+      logdir=args.logdir, client=args.client, max_cpu=max_cpu, current_script=current_script, args=" ".join(argv), num_processes=args.num_processes)
+
+    print(submit)
+
+    p = subprocess.Popen(["condor_submit"], stdin=subprocess.PIPE)
+    p.communicate(submit.encode("utf-8"))
+    rc = p.wait()
+    assert rc == 0
+
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
@@ -443,6 +567,12 @@ if __name__ == "__main__":
     if sys.argv[1] == "update_state":
       update_state(sys.argv[2:])
 
+    if sys.argv[1] == "start_ssh_worker":
+      start_ssh_worker(sys.argv[2:])
+
+    if sys.argv[1] == "start_condor_worker":
+      start_condor_worker(sys.argv[2:])
+
   else:
     print("usage: {} command args".format(sys.argv[0]))
     print("available commands:")
@@ -450,6 +580,8 @@ if __name__ == "__main__":
     print("  refresh_master")
     print("  get_state")
     print("  update_state")
+    print("  start_ssh_worker")
+    print("  start_condor_worker")
     sys.exit(1)
 
 #dag = Dag("test_dag.db", False)
