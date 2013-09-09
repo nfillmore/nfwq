4c0aab3b2fd44d139be2b7268948be0c654b9ebe
Nathanael Fillmore <github@nate-fillmore.com>
2013-09-09 16:21:21 -0500

    Change wq to nfwq in test dag script.

--- make_test_dag.py
+++ make_test_dag.py
@@ -1,21 +1,21 @@
-import sys, wq, work_queue
+import sys, nfwq, work_queue
 
 d = "/scratch/nathanae/test_dag"
 
-dag = wq.Dag(d + "/test_dag.db")
+dag = nfwq.Dag(d + "/test_dag.db")
 dag.init()
 
 dag.add(tag="node1",
         cmd=["touch", "node1.output"],
         files=[{"local_name": d + "/node1.output", 
                 "remote_name": "node1.output", 
-                "type": wq.OUTPUT,
-                "flags": wq.NOCACHE}])
+                "type": nfwq.OUTPUT,
+                "flags": nfwq.NOCACHE}])
 
 dag.add(tag="node2",
         cmd=["ls", "-ltrh", "/ua/nathanae/"],
         files=[{"local_name": d + "/node1.output",
                 "remote_name": "node1.output",
-                "type": wq.INPUT,
-                "flags": wq.NOCACHE}],
+                "type": nfwq.INPUT,
+                "flags": nfwq.NOCACHE}],
         parents=["node1"])
